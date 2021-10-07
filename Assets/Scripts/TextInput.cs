6f3d1491778f942f0e8a717081ad1ba5fed022da
Jimmy DiColandrea <jdicolandrea@gmail.com>
2021-10-07 19:11:45 -0400

    add input

---
+++ Assets/Scripts/TextInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextInput : MonoBehaviour
+{
+    public InputField inputField;
+
+    GameController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<GameController>();
+        inputField.onEndEdit.AddListener(AcceptStringInput);
+    }
+
+    void AcceptStringInput(string userInput)
+    {
+        userInput = userInput.ToLower();
+        controller.LogStringWithReturn(userInput);
+        InputComplete();
+    }
+
+
+    void InputComplete()
+    {
+        controller.DisplayLoggedText();
+        inputField.ActivateInputField();
+        inputField.text = null;
+    }
+}
