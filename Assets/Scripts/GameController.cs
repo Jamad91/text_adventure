d49c7355b145ff01cca13fed0859e53e3dacbbb0
Jimmy DiColandrea <jdicolandrea@gmail.com>
2021-10-06 23:06:45 -0400

    add initial room

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text displayText;
+
+    [HideInInspector] public RoomNavigation roomNavigation;
+
+    List<string> actionLog = new List<string>();
+
+    void Awake()
+    {
+        roomNavigation = GetComponent<RoomNavigation>();
+    }
+
+    public void DisplayLoggedText()
+    {
+        string logAsText = string.Join("\n", actionLog.ToArray());
+
+        displayText.text = logAsText;
+    }
+
+    void Start()
+    {
+        DisplayRoomText();
+        DisplayLoggedText();
+    }
+
+    public void DisplayRoomText()
+    {
+        string combinedText = roomNavigation.currentRoom.description + "\n";
+
+        LogStringWithReturn(combinedText);
+    }
+
+    public void LogStringWithReturn(string stringToAdd)
+    {
+        actionLog.Add(stringToAdd + "\n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RoomNavigation.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNavigation : MonoBehaviour
+{
+    public Room currentRoom;
+}
