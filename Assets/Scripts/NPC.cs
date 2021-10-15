f281ca1b9bbd1b5388be7a6d26f651aaeb2cd423
Jimmy DiColandrea <jdicolandrea@gmail.com>
2021-10-14 22:35:21 -0400

    add basic npc scriptable object

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextAdventure/NPC")]
+public class NPC : ScriptableObject
+{
+
+    public string name = "name";
+    [TextArea]
+    public string description = "Description of person";
+    public Interaction[] interactions;
+
+}
