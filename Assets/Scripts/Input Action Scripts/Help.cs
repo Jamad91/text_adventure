94c8ea78871910df39fdc1728f072a0d44dd4904
Jimmy DiColandrea <jdicolandrea@gmail.com>
2021-10-18 00:54:31 -0400

    display help text

--- Assets/Scripts/Input Action Scripts/Help.cs
+++ Assets/Scripts/Input Action Scripts/Help.cs
@@ -8,6 +8,6 @@ public class Help : InputAction
 {
     public override void RespondToInput(GameController controller, string[] seperatedInputWords)
     {
-        controller.DisplayHelp();
+        controller.LogStringWithReturn("Need some help? You can:\n GO in a dicrection \n TAKE something \n EXAMINE something \n check your INVENTORY \n TALK to someone");
     }
 }
