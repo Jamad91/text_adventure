34f3fca42af002c54976c8259e638d19d0dda956
Jimmy DiColandrea <jdicolandrea@gmail.com>
2021-12-28 11:45:32 -0500

    adjust item information

---
+++ Temp/StagingArea/Data/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,49 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+extern Il2CppGenericClass* const s_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions s_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+extern void** const g_MetadataUsages[];
+extern const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	1206,
+	s_Il2CppGenericTypes,
+	1063,
+	g_Il2CppGenericInstTable,
+	12018,
+	s_Il2CppGenericMethodFunctions,
+	12522,
+	g_Il2CppTypeTable,
+	12817,
+	g_Il2CppMethodSpecTable,
+	3557,
+	g_FieldOffsetTable,
+	3557,
+	g_Il2CppTypeDefinitionSizesTable,
+	13838,
+	g_MetadataUsages,
+};
